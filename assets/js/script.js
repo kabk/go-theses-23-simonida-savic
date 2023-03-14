978da2332ce760d5d54bbd4a35ca286b4c300b4e
graphicMatters <107033621+graphicMatters@users.noreply.github.com>
2023-03-14 16:44:53 +0100

    changes_4.44

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,3 +70,15 @@ document.body.onmousemove = function(e) {
     + 'px'
   );
 }
+const cursorBig = document.querySelector('.big');
+
+
+const positionElement = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+  
+  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+}
+
+window.addEventListener('mousemove', positionElement)

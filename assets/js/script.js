472e648bcc436b49da47493f8d9ac399d35b0d95
graphicMatters <107033621+graphicMatters@users.noreply.github.com>
2023-03-14 12:37:21 +0100

    14_MARCH_2023_01

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,37 @@ window.addEventListener( 'resize', ( ) => {
 window.dispatchEvent( new Event( 'resize' ) );
 
 littlefoot.default( );
+
+let mybutton = document.getElementById("floating-windows");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}

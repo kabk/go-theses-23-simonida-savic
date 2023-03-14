c6ae9cb1775618a1bc37cee9e8bfb2f67ba55e92
graphicMatters <107033621+graphicMatters@users.noreply.github.com>
2023-03-14 18:44:59 +0100

    changes_6.44

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,24 +13,41 @@ window.dispatchEvent( new Event( 'resize' ) );
 
 littlefoot.default( );
 
-let mybutton = document.getElementById("floating-windows");
+
+const menuBtns = document.querySelectorAll( 'nav ul li a' );
+menuBtns.forEach( btn => {
+  btn.addEventListener( 'click', ( ) => {
+    const windows = document.querySelectorAll( '.window' );
+    windows.forEach( window => {
+      window.scrollTop = 0;
+    } );
+  } );
+} );
+
+const backToTopBtns = document.querySelectorAll( '.btn-back-to-top' );
+backToTopBtns.forEach( btn => {
+  btn.addEventListener( 'click', ( ) => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  } );
+} );
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//   //   mybutton.style.display = "block";
+//   // } else {
+//   //   mybutton.style.display = "none";
+//   // }
+// }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
@@ -47,29 +64,34 @@ for (i = 0; i < coll.length; i++) {
   });
 } 
 
-var scrollElement = document.querySelector('#scroll');
+const scrollElement = document.querySelector('#scroll');
+const floatingWindows = document.getElementById( 'floating-windows' );
 
 window.addEventListener('scroll', function(){
-  var height = document.body.scrollHeight - this.innerHeight;
-  var scrolledPixels = this.scrollY;
+  const scrollStartY = floatingWindows.offsetTop;
+
+  var height = document.body.scrollHeight - this.innerHeight - scrollStartY;
+  var scrolledPixels = this.scrollY - scrollStartY;
   var width = ((scrolledPixels / height) * 100).toFixed(2);
+  
   scrollElement.style.width = width + '%';
 });
 
-document.body.onmousemove = function(e) {
-  document.documentElement.style.setProperty (
-    '--x', (
-      e.clientX+window.scrollX
-    )
-    + 'px'
-  );
-  document.documentElement.style.setProperty (
-    '--y', (
-      e.clientY+window.scrollY
-    ) 
-    + 'px'
-  );
-}
+// document.body.onmousemove = function(e) {
+//   document.documentElement.style.setProperty (
+//     '--x', (
+//       e.clientX+window.scrollX
+//     )
+//     + 'px'
+//   );
+//   document.documentElement.style.setProperty (
+//     '--y', (
+//       e.clientY+window.scrollY
+//     ) 
+//     + 'px'
+//   );
+// }
+
 const cursorBig = document.querySelector('.big');
 
 
@@ -77,7 +99,7 @@ const positionElement = (e)=> {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
   
-  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  cursorBig.style.transform = `translate3d(calc(${mouseX}px - 50%), calc(${mouseY}px - 50%), 0)`;
  
 }
 

57f3f1e0dce64457828068e789fd0cce6af0a84a
graphicMatters <107033621+graphicMatters@users.noreply.github.com>
2023-03-14 13:25:09 +0100

    scroll_bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,3 +46,12 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+var scrollElement = document.querySelector('#scroll');
+
+window.addEventListener('scroll', function(){
+  var height = document.body.scrollHeight - this.innerHeight;
+  var scrolledPixels = this.scrollY;
+  var width = ((scrolledPixels / height) * 100).toFixed(2);
+  scrollElement.style.width = width + '%';
+});

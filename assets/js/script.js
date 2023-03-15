7a1eb3c400e3a00aa621ff1b0c791cfab30776db
graphicMatters <107033621+graphicMatters@users.noreply.github.com>
2023-03-15 17:29:08 +0100

    vertical_indicator

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,26 @@ window.dispatchEvent( new Event( 'resize' ) );
 
 littlefoot.default( );
 
+const progressBar = document.getElementById("progressbar");
+progressBar.style.height = 1 + "%";
+
+window.onscroll = () => {
+	const scroll = document.documentElement.scrollTop;
+	const height =
+		document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	let scrolled = (scroll / height) * 100;
+
+	if (scrolled <= 1) {
+		progressBar.style.height = 1 + "%";
+	} else if (scrolled >= 2 && scrolled <= 99.9) {
+		progressBar.style.height = scrolled + "%";
+		progressBar.classList.remove("glow");
+	} else if (scrolled === 100) {
+		progressBar.style.height = scrolled + "%";
+		// 		Do something when reached 100% scroll
+		progressBar.classList.add("glow");
+	}
+};
 
 const menuBtns = document.querySelectorAll( 'nav ul li a' );
 menuBtns.forEach( btn => {
